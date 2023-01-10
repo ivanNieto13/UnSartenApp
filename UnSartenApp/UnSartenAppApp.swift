b3ed18f00fc5ad2f0acf319a4d71710d9abf64c3
Ivan Nieto <ivannieto59@aragon.unam.mx>
2023-01-09 18:52:47 -0600

    Initial Commit

---
+++ UnSartenApp/UnSartenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSartenAppApp.swift
+//  UnSartenApp
+//
+//  Created by Ivan Nieto on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UnSartenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
