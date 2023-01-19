8eacba2b819763cc2ce0cdcd29c28cda8e6ebe4e
Ivan Nieto <ivannieto59@aragon.unam.mx>
2023-01-19 17:57:13 -0600

    feat: add enum + json null to shared libs

---
+++ UnSartenApp/model/shared/URLError.swift
@@ -0,0 +1,12 @@
+//
+// Created by Ivan Nieto on 19/01/23.
+//
+
+import Foundation
+
+enum URLError: LocalizedError {
+    case unauthorised
+    case timeout
+    case serverError
+    case invalidResponse
+}
