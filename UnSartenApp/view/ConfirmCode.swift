2d9542fb696076148a750354f9b8f7a5c0e95a8b
Ivan Nieto <ivannieto59@aragon.unam.mx>
2023-01-19 18:00:41 -0600

    feat: add ConfirmCode view

--- UnSartenApp/view/ConfirmCode.swift
+++ UnSartenApp/view/ConfirmCode.swift
@@ -8,20 +8,27 @@
 import SwiftUI
 
 struct ConfirmCodeContentView: View {
+    @StateObject private var verifyCodeViewModel = VerifyCodeViewModel()
+    @StateObject private var saveUserModel = SaveUserDataViewModel()
+
     @State var showingLoginScreen = false
     @State var showingAlert = false
+    @State var showAlert = false
     @State var showingSendButtonActive = false
 
     @Binding var path: NavigationPath
-    @Binding var phoneNumber: String
     @Binding var code: String
+    @Binding var verifyNumber: VerifyNumber
+    @Binding var verifyCode: VerifyCode
+
+    @Binding var confirmCode: Bool
 
     @Environment(\.presentationMode) var presentation
     @FocusState private var keyboardFocused: Bool
 
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
-            Text("Ingresa el código de 4 dígitos que se te envió a +52 \(phoneNumber)")
+            Text("Ingresa el código de 4 dígitos que se te envió a \(verifyNumber.data.verifyNumber.phoneNumber)")
                     .font(.title)
                     .bold()
                     .foregroundColor(Color("IconColor"))
@@ -31,6 +38,7 @@ struct ConfirmCodeContentView: View {
                     .font(.title3)
                     .bold()
                     .frame(width: 300, alignment: .leading)
+
             TextField("1234", text: $code)
                     .padding()
                     .frame(width: 300, height: 50)
@@ -63,8 +71,15 @@ struct ConfirmCodeContentView: View {
                     })
 
             Button(action: {
+                DispatchQueue.main.async {
+                    verifyCodeViewModel.isLoading = true
+                    verifyCodeViewModel.verifyCode(code: code)
+                }
                 showingAlert = true
-                path.append("RegisterUser")
+                // if user is not registered, register user
+                // if user is registered, go to main screen
+                // call api to fetch orders data and list them
+                // path.append("MainScreen")
             }) {
                 switch showingSendButtonActive {
                 case true:
@@ -100,6 +115,7 @@ struct ConfirmCodeContentView: View {
 
             Button("Cambiar teléfono") {
                 presentation.wrappedValue.dismiss()
+                confirmCode = false
             }
                     .fontWeight(.bold)
                     .font(.body)
@@ -113,9 +129,36 @@ struct ConfirmCodeContentView: View {
                                     .stroke(Color("TertiaryColor"), lineWidth: 1)
                     )
 
+            Button("Code") {
+
+            }
+                    .opacity(0)
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                                title: Text("Codigo Invalido"),
+                                message: Text("El codigo que ingresaste es incorrecto, intenta nuevamente."))
+                    }
+                    .onDisappear {
+                        showAlert = false
+                    }
+
             Spacer()
 
         }
+                .onChange(of: verifyCodeViewModel.verifyCode.data.verifyCode.code != "" && saveUserModel.isLoading == false, perform: { value in
+                    if value != nil {
+                        verifyCode = verifyCodeViewModel.verifyCode
+                        if verifyCode.data.verifyCode.isValid == true {
+                            path.append("RegisterUser")
+                        } else {
+                            showAlert = true
+                        }
+                    }
+                })
+                .onAppear {
+                    print("ConfirmCodeContentView onAppear", verifyNumber.data.verifyNumber)
+
+                }
                 .padding()
                 .navigationBarBackButtonHidden(true)
 
@@ -124,6 +167,9 @@ struct ConfirmCodeContentView: View {
 
 struct ConfirmCodeContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ConfirmCodeContentView(path: .constant(NavigationPath()), phoneNumber: .constant(""), code: .constant(""))
+        ConfirmCodeContentView(path: .constant(NavigationPath()),
+                code: .constant(""),
+                verifyNumber: .constant(VerifyNumber(data: DataClass(verifyNumber: VerifyNumberClass(phoneNumber: "", isVerified: false, userID: nil), error: nil))),
+                verifyCode: .constant(VerifyCode(data: VCDataClass(verifyCode: VerifyCodeClass(code: "", isValid: true), error: nil))), confirmCode: .constant(false))
     }
 }
